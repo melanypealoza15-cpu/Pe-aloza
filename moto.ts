95fd8b442abe4026c1d48902d76f3ea61a7c50d8
melanypealoza15-cpu <melanypealoza15@gmail.com>
2026-05-28 11:25:55 -0500

    Create moto.ts
    .

---
+++ moto.ts
@@ -0,0 +1,6 @@
+class Moto extends Vehiculo{
+    llantas!: string
+    Motor!: string
+}
+verificarLicencia(tipoMotor: string): string
+return "Necesitas una licencia para motos de alta cilindrada.";
